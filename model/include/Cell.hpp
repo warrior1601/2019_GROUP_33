4e266856c945c42d8c6104924d0097415261def3
warrior1601 <eeyjqp@nottingham.ac.uk>
2020-03-20 19:35:50 +0000

    Travis test

--- model/include/Cell.hpp
+++ model/include/Cell.hpp
@@ -25,7 +25,7 @@
 #include "Vectors.h"
 #include <vector>
 /**
- * @class Cell cell.hpp "cell.hpp"
+ * @class Cell Cell.hpp "Cell.hpp"
  * @brief The Cell class is the parent class for all shapes
  * that can be created with this library
  */
@@ -112,7 +112,7 @@ private:
 
 
 /**
- * @class Tetrahedron cell.hpp "cell.hpp"
+ * @class Tetrahedron Cell.hpp "Cell.hpp"
  * @brief The Tetrahedron class is a subclass of the Cell class
  * that has 4 Vectors.
  * \image html tetrahedron.png " Tetrahedron Vector numbering" width=500cm
@@ -166,7 +166,7 @@ public:
 
 
 /**
- * @class Pyramid cell.hpp "cell.hpp"
+ * @class Pyramid Cell.hpp "Cell.hpp"
  * @brief The Pyramid class is a subclass of the Cell class
  * that has 5 Vectors.
  * \image html pyramid.png " Pyramid Vector numbering" width=500cm
@@ -218,7 +218,7 @@ public:
 
 
 /**
- * @class Hexahedron cell.hpp "cell.hpp"
+ * @class Hexahedron Cell.hpp "Cell.hpp"
  * @brief The Hexahedron class is a subclass of the Cell class
  * that has 8 Vectors.
  * \image html hexahedron.png " Hexahedron Vector numbering" width=500cm
