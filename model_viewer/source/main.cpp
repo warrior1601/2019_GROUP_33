ce1d8dbb19eabd8c8d2872506f0f78a540b3c66a
warrior1601 <eeyjqp@nottingham.ac.uk>
2020-04-22 11:33:28 +0100

    Console now hidden while running application

--- model_viewer/source/main.cpp
+++ model_viewer/source/main.cpp
@@ -2,13 +2,13 @@
 #include <QApplication>
 #include <QSurfaceFormat>
 #include <QVTKOpenGLWidget.h>
-
-
-
+#include <windows.h>
 #include "mainwindow.h"
 
 int main( int argc, char** argv )
 {
+  //This hides the console window for the final applicaation
+  FreeConsole();
   // needed to ensure appropriate OpenGL context is created for VTK rendering.
   QSurfaceFormat::setDefaultFormat( QVTKOpenGLWidget::defaultFormat() );
 
